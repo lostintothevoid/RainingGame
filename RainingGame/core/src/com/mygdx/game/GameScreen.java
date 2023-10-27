38fec1b14aa30ea75466337716cb288a49c4458e
lostintothevoid <127423332+lostintothevoid@users.noreply.github.com>
2023-10-27 15:59:04 -0700

    Revert "test"
    This reverts commit d1e880be6aa2552c0cf208a7349c8791f3c3cb44.

--- RainingGame/core/src/com/mygdx/game/GameScreen.java
+++ RainingGame/core/src/com/mygdx/game/GameScreen.java
@@ -25,24 +25,32 @@ public class GameScreen implements Screen {
 		this.game = game;
         this.batch = game.getBatch();
         this.font = game.getFont();
-		  
-          tarro = new Tarro();
-          // creacion del tarro
-	      tarro.crear();   
-	     
-	      lluvia = new Lluvia();
-	      // creacion de la lluvia
-	      lluvia.crear(); 
+		  // load the images for the droplet and the bucket, 64x64 pixels each 	     
+		  Sound hurtSound = Gdx.audio.newSound(Gdx.files.internal("hurt.ogg"));
+		  tarro = new Tarro(new Texture(Gdx.files.internal("bucket.png")),hurtSound);
+         
+	      // load the drop sound effect and the rain background "music" 
+         Texture gota = new Texture(Gdx.files.internal("drop.png"));
+         Texture gotaMala = new Texture(Gdx.files.internal("dropBad.png"));
+         
+         Sound dropSound = Gdx.audio.newSound(Gdx.files.internal("drop.wav"));
+        
+	     Music rainMusic = Gdx.audio.newMusic(Gdx.files.internal("rain.mp3"));
+         lluvia = new Lluvia(gota, gotaMala, dropSound, rainMusic);
 	      
+	      // camera
 	      camera = new OrthographicCamera();
 	      camera.setToOrtho(false, 800, 480);
 	      batch = new SpriteBatch();
+	      // creacion del tarro
+	      tarro.crear();
+	      
+	      // creacion de la lluvia
+	      lluvia.crear();
 	}
 
 	@Override
 	public void render(float delta) {
-		
-		
 		//limpia la pantalla con color azul obscuro.
 		ScreenUtils.clear(0, 0, 0.2f, 1);
 		//actualizar matrices de la cámara
@@ -55,20 +63,20 @@ public class GameScreen implements Screen {
 		font.draw(batch, "Vidas : " + tarro.getVidas(), 670, 475);
 		font.draw(batch, "HighScore : " + game.getHigherScore(), camera.viewportWidth/2-50, 475);
 		
-	    if(!tarro.actualizarMovimiento()) {
-	    	if (!lluvia.actualizarMovimiento(tarro)) {
-		    	  //actualizar HigherScore
-		    	if (game.getHigherScore() < tarro.getPuntos()) {
-		    		game.setHigherScore(tarro.getPuntos());  
-		    	}
-		    	//ir a la ventana de finde juego y destruir la actual
-		    	if(!tarro.estaHerido()) {
-		    		game.setScreen(new GameOverScreen(game));
-		    	  	dispose();
-		    	}
-		    }
-	    }
-	    // Se dibuja el tarro y la lluvia
+		if (!tarro.estaHerido()) {
+			// movimiento del tarro desde teclado
+	        tarro.actualizarMovimiento();        
+			// caida de la lluvia 
+	       if (!lluvia.actualizarMovimiento(tarro)) {
+	    	  //actualizar HigherScore
+	    	  if (game.getHigherScore()<tarro.getPuntos())
+	    		  game.setHigherScore(tarro.getPuntos());  
+	    	  //ir a la ventana de finde juego y destruir la actual
+	    	  game.setScreen(new GameOverScreen(game));
+	    	  dispose();
+	       }
+		}
+		
 		tarro.dibujar(batch);
 		lluvia.actualizarDibujoLluvia(batch);
 		
